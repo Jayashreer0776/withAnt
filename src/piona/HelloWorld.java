7400c30a7f574ac92d77f8f9484733b86770f73d
Jayashree <Jayashree.r@sonata-software.com>
2020-01-30 17:50:46 +0530

    Changed on 30 Jan

--- src/piona/HelloWorld.java
+++ src/piona/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * HelloWorld constructor.
      */
     public HelloWorld() {
-        greetings = "Hello! Welcome to Jenkins!!!";
+        greetings = "Hello! ";
     }
 
     /**
